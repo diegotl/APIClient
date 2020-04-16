94df1f7125e62c9e968b72ed45e28eaff3721133
Diego Trevisan Lara <diego@trevisa.nl>
2020-04-16 15:06:24 +0200

    APIClientOkLog target

--- Package.swift
+++ Package.swift
@@ -6,14 +6,14 @@ let package = Package(
     name: "APIClient",
     products: [
         .library(name: "APIClient", targets: ["APIClient"]),
-        .library(name: "APIClient+OkLog", targets: ["APIClient+OkLog"]),
+        .library(name: "APIClientOkLog", targets: ["APIClientOkLog"]),
     ],
     dependencies: [
         .package(url: "https://github.com/diegotl/OkLog-Swift.git", from: "0.2.4"),
     ],
     targets: [
         .target(name: "APIClient", path: "Sources/Core"),
-        .target(name: "APIClient+OkLog", dependencies: ["OkLog"], path: "Sources/OkLog"),
+        .target(name: "APIClientOkLog", dependencies: ["OkLog"], path: "Sources/OkLog"),
         .testTarget(name: "APIClientTests", dependencies: ["APIClient"]),
     ]
 )
